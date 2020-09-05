fc44704ecb927f0a8b28be35e6e601f5b080727b
iiiplay001@gmail.com <Me09235168>
2020-09-05 15:31:46 +0800

    chapter02 BMI佈局更新

--- chapter02/app/src/main/java/me/app17/chapter02/MainActivity.java
+++ chapter02/app/src/main/java/me/app17/chapter02/MainActivity.java
@@ -3,12 +3,32 @@ package me.app17.chapter02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editSex;
+    EditText editAge;
+    Button btnBMI;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.bmi_layout);
+//
+//        editSex = (EditText) findViewById(R.id.edit_sex);
+//        editAge = (EditText) findViewById(R.id.edit_age);
+//        btnBMI = (Button) findViewById(R.id.btn_bmi);
+//
+//        String sex = editSex.getText().toString();
+//        int age = Integer.valueOf(editAge.getText().toString());
+        //float bmi=Double.valueOf()
+
+        //Toast.makeText(this, )
+
+
     }
 }
