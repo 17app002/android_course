dc514653f4831699bc525267f59d2137905f63be
iiiplay001@gmail.com <Me09235168>
2020-09-16 12:44:55 +0800

    更新thsViewer讀取等功能

--- thsViewer/app/src/main/java/me/app17/thsviewer/MainActivity.java
+++ thsViewer/app/src/main/java/me/app17/thsviewer/MainActivity.java
@@ -1,26 +1,47 @@
 package me.app17.thsviewer;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.List;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ListView itemLv;
+    Button getBtn;
     Button updateBtn;
+    TextView countTv;
+
 
     public static MainActivity instance;
     public static Handler uiHandler;
+
+    private THSViewer thsViewer;
+
+    final class UIHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                //資料庫資料讀取完畢
+                case 1:
+                    countTv.setText(String.valueOf(thsViewer.getListCount()));
+                    updateBtn.setEnabled(true);
+                    break;
+            }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,22 +49,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         instance = this;
-        uiHandler = new Handler();
+        uiHandler = new UIHandler();
 
         findViews();
+        thsViewer = new THSViewer(itemLv);
 
-        updateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                itemLv.setAdapter(null);
-                new Thread(new THSViewer(itemLv)).start();
-            }
-        });
+        //showLoadingDialog();
     }
 
     public void findViews() {
         itemLv = findViewById(R.id.item_lv);
+        getBtn = findViewById(R.id.get_btn);
         updateBtn = findViewById(R.id.update_btn);
+        countTv = findViewById(R.id.count_tv);
+        getBtn.setOnClickListener(this);
+        updateBtn.setOnClickListener(this);
+
+        updateBtn.setEnabled(false);
     }
 
     /***
@@ -51,14 +73,53 @@ public class MainActivity extends AppCompatActivity {
      * @param msg
      */
     //https://codertw.com/android-%E9%96%8B%E7%99%BC/352565/
-    public static void showMessage(final String msg){
+    public static void showMessage(final String msg) {
         uiHandler.post(new Runnable() {
             @Override
             public void run() {
                 Toast.makeText(instance.getApplicationContext(),
-                        msg,Toast.LENGTH_SHORT).show();
+                        msg, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        //取得資料按鈕
+        if (v.getId() == R.id.get_btn) {
+            getBtn.setEnabled(false);
+            if (thsViewer.getMapList() != null) {
+                Toast.makeText(getApplicationContext(), "目前已經是最新資料", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            itemLv.setAdapter(null);
+            thsViewer.update();
+            return;
+        }
+
+        //更新資料
+        if (v.getId() == R.id.update_btn) {
+            new AlertDialog.Builder(this)
+                    .setTitle("更新資料?")
+                    .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            itemLv.setAdapter(null);
+                            thsViewer.update();
+                        }
+                    }).setNegativeButton("取消", null).create()
+                    .show();
+
+            return;
+        }
+    }
+
+    public void showLoadingDialog() {
+
+
+
     }
 }
 
--- thsViewer/app/src/main/java/me/app17/thsviewer/THSViewer.java
+++ thsViewer/app/src/main/java/me/app17/thsviewer/THSViewer.java
@@ -5,10 +5,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.Toast;
 
-import me.app17.thsviewer.util.JDBCUtil;
-
+import androidx.appcompat.app.AlertDialog;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -19,8 +17,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import me.app17.thsviewer.util.JDBCUtil;
 
-public class THSViewer implements Runnable {
+
+public class THSViewer {
 
     final class ThsData {
         String date;
@@ -33,16 +33,36 @@ public class THSViewer implements Runnable {
     }
 
     //資料庫對應欄位名稱
-    String[] columns = {"no", "date", "start_loc", "end_loc", "start_time", "end_time", "price"};
-    ListView listView;
+    private String[] columns = {"no", "date", "start_loc", "end_loc", "start_time", "end_time", "price"};
+    private ListView listView;
+    private List<Map<String, Object>> mapList;
+
+
+    private static AlertDialog loadingDialog;
 
     public THSViewer(View view) {
         listView = (ListView) view;
     }
 
+    /***
+     * 取得mapList
+     * @return
+     */
+    public List<Map<String, Object>> getMapList() {
+        return mapList;
+    }
+
+    /***
+     * 取得資料長度
+     * @return
+     */
+    public int getListCount() {
+        return mapList.size();
+    }
 
     /**
      * 取得資料庫資料
+     *
      * @param resultSet
      * @param length
      * @return
@@ -99,7 +119,7 @@ public class THSViewer implements Runnable {
         return mapList;
     }
 
-     /***
+    /***
      * 更新listView
      * @param mapList
      */
@@ -131,47 +151,79 @@ public class THSViewer implements Runnable {
         return connection;
     }
 
+
     /***
-     * 執行線程
+     * 更新資料
      */
-    @Override
-    public void run() {
-        Connection connection = getConnection();
-        if (connection == null) {
-            MainActivity.showMessage("連線失敗!");
-            return;
-        }
-        Log.i("info", "連線成功，取得資料中...");
+    public void update() {
 
-        try {
-            Statement statement = connection.createStatement();
-            String sqlStr = "select * from ths_data;";
-            ResultSet resultSet = statement.executeQuery(sqlStr);
-            if (resultSet == null) {
-                MainActivity.showMessage("目前無資料");
-                return;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //產生loadingBar
+                MainActivity.uiHandler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.instance);
+                        dialog.setView(MainActivity.instance.getLayoutInflater().inflate(R.layout.ths_loading_layout, null));
+                        loadingDialog = dialog.create();
+                        loadingDialog.setCanceledOnTouchOutside(false);
+                        loadingDialog.show();
+                    }
+                });
+
+
+                Connection connection = getConnection();
+                if (connection == null) {
+                    loadingDialog.dismiss();
+                    MainActivity.showMessage("連線失敗!");
+                    return;
+                }
+                Log.i("info", "連線成功，取得資料中...");
+
+                try {
+                    Statement statement = connection.createStatement();
+                    String sqlStr = "select * from ths_data;";
+                    ResultSet resultSet = statement.executeQuery(sqlStr);
+                    if (resultSet == null) {
+                        loadingDialog.dismiss();
+                        MainActivity.showMessage("目前無資料");
+
+                        return;
+                    }
+                    //取得目前共幾筆資料
+                    resultSet.last();
+                    MainActivity.showMessage("共" + resultSet.getRow() + "筆資料");
+
+                    //移動到資料最前面
+                    resultSet.beforeFirst();
+                    List<ThsData> thsDataList = getDbData(resultSet, 100);
+                    //刷新ListView
+                    if (thsDataList.size() > 0) {
+                        mapList = getListData(thsDataList);
+                        updateListView(mapList);
+                    }
+
+                    loadingDialog.dismiss();
+                    //接收資料庫完成
+                    Message message = new Message();
+                    message.what = 1;
+                    MainActivity.uiHandler.sendMessage(message);
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        connection.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
 
-            //取得目前共幾筆資料
-            resultSet.last();
-            MainActivity.showMessage("共" + resultSet.getRow() + "筆資料");
-            //移動到資料最前面
-            resultSet.beforeFirst();
-            List<ThsData> thsDataList = getDbData(resultSet, 100);
-
-            //刷新ListView
-            if (thsDataList.size() > 0) {
-                updateListView(getListData(thsDataList));
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        }).start();
     }
 }
+
+
