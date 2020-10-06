96744552354ad68521f8af66beb46cd0922de871
iiiplay001@gmail.com <Me09235168>
2020-10-06 17:36:30 +0800

    ch3 update ver1   程式碼還沒處理好

---
+++ ch3/LoginDemo/app/src/main/java/me/app17/logindemo/MainActivity.java
@@ -0,0 +1,17 @@
+package me.app17.logindemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
