e9267a216992411cc4ab03d59e260974152d1435
iiiplay001@gmail.com <Me09235168>
2020-10-13 17:14:27 +0800

    ch6 GridLayout update(計算機功能)

---
+++ ch6/Calculator/app/src/main/java/me/app17/calculator/MainActivity.java
@@ -0,0 +1,130 @@
+package me.app17.calculator;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView numberText;
+    //紀錄是否重新輸入
+    private boolean newStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_1);
+        numberText = findViewById(R.id.number_text);
+        numberText.setMaxLines(3);
+        newStart = true;
+
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.clear_btn:
+                clear();
+                break;
+            case R.id.n0_btn:
+                addNumberText("0");
+                break;
+            case R.id.n1_btn:
+                addNumberText("1");
+                break;
+            case R.id.n2_btn:
+                addNumberText("2");
+                break;
+            case R.id.n3_btn:
+                addNumberText("3");
+                break;
+            case R.id.n4_btn:
+                addNumberText("4");
+                break;
+            case R.id.n5_btn:
+                addNumberText("5");
+                break;
+            case R.id.n6_btn:
+                addNumberText("6");
+                break;
+            case R.id.n7_btn:
+                addNumberText("7");
+                break;
+            case R.id.n8_btn:
+                addNumberText("8");
+                break;
+            case R.id.n9_btn:
+                addNumberText("9");
+                break;
+            case R.id.add_btn:
+                addNumberText("+");
+                break;
+            case R.id.dec_btn:
+                addNumberText("-");
+                break;
+            case R.id.multi_btn:
+                addNumberText("*");
+                break;
+            case R.id.dot_btn:
+                addNumberText(".");
+                break;
+            case R.id.div_btn:
+                addNumberText("/");
+                break;
+            case R.id.ans_btn:
+                answer();
+                break;
+        }
+    }
+
+    /***
+     * 引入JavaScript engine
+     */
+    public void answer() {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
+        try {
+            Object result = (double) engine.eval(numberText.getText().toString());
+            numberText.setGravity(Gravity.RIGHT|Gravity.BOTTOM);
+            newStart = true;
+
+            //檢查小數點尾數是否是0
+            String value=String.valueOf(result);
+            String[] temp=value.split("\\.");
+
+            if(temp[1].equals("0")){
+                result=temp[0];
+            }
+
+            numberText.setText(result.toString());
+            Log.d("ManActivity", result.toString());
+
+        } catch (ScriptException e) {
+            Toast.makeText(this, "計算錯誤!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void clear() {
+        numberText.setText("0");
+        newStart = true;
+        numberText.setGravity(Gravity.LEFT);
+    }
+
+    public void addNumberText(String number) {
+        //去除第一個零
+        if (newStart) {
+            numberText.setText("");
+            numberText.setGravity(Gravity.LEFT);
+            newStart = false;
+        }
+        numberText.setText(numberText.getText() + number);
+    }
+}
